d1b66c9437ccd1101ecb32a397e65d4427bbf9c7
Sourabh Mane <msourabh631@gmail.com>
2023-06-29 23:55:59 +0530

    Initial commit

---
+++ src/test/java/com/geekster/CoffeeVendorMachine/CoffeeVendorMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.CoffeeVendorMachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeVendorMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
